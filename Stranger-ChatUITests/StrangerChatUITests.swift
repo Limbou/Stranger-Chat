6551a26083f6fd7ec8d94415d70acf4be1f4c86a
Jakub Danielczyk <kuba@danielczyk.pl>
2018-11-19 15:24:20 +0100

    Rename test classes so it passes SwiftLint build.

--- Stranger-ChatUITests/StrangerChatUITests.swift
+++ Stranger-ChatUITests/StrangerChatUITests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class Stranger_ChatUITests: XCTestCase {
+class StrangerChatUITests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,8 +18,6 @@ class Stranger_ChatUITests: XCTestCase {
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDown() {
